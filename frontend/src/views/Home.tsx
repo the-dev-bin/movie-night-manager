43ec9c1f955848a0b9226f1fe3551e8363b55b42
tylercchase <tylercchase@gmail.com>
2023-04-23 02:08:02 -0400

    feat: home page formatting

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,11 +1,22 @@
+import {
+	AppShell, Center, Button, Stack, Title
+} from "@mantine/core";
 import { Link } from "react-router-dom";
 
 
 
 function Home() {
 	return <>
-		Hello Homepage
-		<Link to={`/group/suggest`}>Suggest a Movie</Link>
+		<AppShell>
+			<Center style={{height: '100%'}}>
+				<Stack>
+					<Title order={1}>Hello Homepage</Title>
+					<Button component={Link} to={'/group/suggest'}>
+						Login
+					</Button>
+				</Stack>
+			</Center>
+		</AppShell>
 	</>
 }
 
