c188fa132a9ccc6e5360c7671241c719f5a7e2b8
tylercchase <tylercchase@gmail.com>
2023-04-23 12:41:37 -0400

    feat: generic group layout

--- frontend/src/views/Group.tsx
+++ frontend/src/views/Group.tsx
@@ -1,9 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { IconGhostFilled } from '@tabler/icons-react';
+import {
+	AppShell,
+	Header,
+	Group,
+	Title,
+	ThemeIcon
+} from "@mantine/core";
 
 
 function GroupLayout() {
 	return <>
-		<Outlet />
+		<AppShell
+			header={
+				<Header height={{ base: 80}} p="md">
+					<Group grow align="center" h="100%">
+						<Group align="center">
+							<ThemeIcon variant="outline" radius="xl" color="orange" >
+								<IconGhostFilled />
+							</ThemeIcon>
+							<Title><Link to={'/'}>Shiver</Link></Title>
+						</Group>
+						<Group position="center">
+							<Link to={`/group/suggest`}>Suggest</Link>
+							<Link to={`/group/manage`}>Manage</Link>
+						</Group>
+					</Group>
+				</Header>
+			}
+		>
+			<Outlet />
+		</AppShell>
 	</>
 }
 
